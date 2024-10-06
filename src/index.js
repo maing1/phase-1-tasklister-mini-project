b6f09c15bc63ee3851574268d6a415c37a1e1bf6
maing1 <faithmaingi1622@gmail.com>
2024-10-06 23:45:27 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDo(e.target.new-task-description.value)
+  form.reset
+})
+
+
+function buildToDo(toDo){
+  let done = document.createElement('p')
+  let button =document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = 'X'
+  done.textContent = `${toDo}`
+  done.appendChild(button)
+  console.log(done)
+   document.querySelector('#list').appendChild(done)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
 });
+
+
